14a4e482a1496335a6663c1a63e18d1c606731d8
Balázsi Bence Márk <balazsi.bence110@gmail.com>
2025-12-01 17:13:13 +0100

    UserServiceImpl

--- src/main/java/com/projekt/koriker/data/repository/UserRepository.java
+++ src/main/java/com/projekt/koriker/data/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.projekt.koriker.data.repository;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projekt.koriker.data.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity,Long>{
-    UserEntity findByUsername(String username);
+    Optional<UserEntity> findByUsername(String username);
     
 } 
     
---
+++ src/main/java/com/projekt/koriker/service/impl/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.projekt.koriker.service.impl;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.projekt.koriker.data.entity.UserEntity;
+import com.projekt.koriker.data.repository.UserRepository;
+import com.projekt.koriker.service.UserService;
+import com.projekt.koriker.service.dto.UserDto;
+import com.projekt.koriker.service.mapper.UserMapper;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+    
+    public void registerUser (UserDto userDto){
+        UserEntity user=userMapper.toEntity(userDto);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        if( user.getRole()==null||user.getRole().isEmpty()){
+            user.setRole("USER");
+        }
+        userRepository.save(user);
+    }
+    @Override
+    public UserDto findByUsername(String username) {
+        UserEntity user = userRepository.findByUsername(username).orElse(null);
+        return userMapper.toDto(user);
+    }
+    
+}
